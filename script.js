73270a19bdc79c0ed0a8063e546b149c55ec8dfc
Reach <brianelliott2015@gmail.com>
2023-06-02 20:35:48 -0400

    Adjust UI sizes, implement clear and operators
    Changed button font size, implemented clear function
    and attached an event listener to the button, same
    with the operator buttons.

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let leftNum;
 let rightNum;
 let operator;
+let opSet = false;
 
 const display = document.querySelector("#display");
+
 const numbers = document.querySelectorAll(".num");
 numbers.forEach(num=> num.addEventListener("click",concatDigit));
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearDisplay);
+
+const operators = document.querySelectorAll(".oper");
+operators.forEach(num=> num.addEventListener("click",setOperator));
 
 function add(a,b) { return a + b; }
 function subtract(a,b) { return a - b; }
 function multiply(a,b) { return a * b; }
-function divide(a,b) { return a / b; }
+function divide(a,b) { 
+    if(b===0)
+        return "Divide by zero"
+    return a / b; 
+}
 
 function operate(op, a, b) {
     switch(op){
@@ -30,4 +42,17 @@ function concatDigit(e){
     const pressed = e.target;
     let number = pressed.value;
     display.innerHTML = display.innerHTML + number;
+}
+
+function clearDisplay(e) {
+    leftNum = undefined;
+    rightNum = undefined;
+    operator = undefined;
+    opSet = false;
+    display.innerHTML = "";
+}
+
+function setOperator(e) {
+    const pressed = e.target;
+    operator = pressed.value;
 }
