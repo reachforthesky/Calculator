bba915d15c37fd8b159ffeaa2542723d64126290
Reach <brianelliott2015@gmail.com>
2023-06-03 15:54:37 -0400

    Implement backspace and fix A LOT of bugs
    Backspace button now correctly deletes right-most
    digit. Fixed bugs where backspace would delete too
    many digits, no digits, bugs where pressing an
    operator button would cause state to be incorrect
    or cause displayed number to dissapear, bug where
    pressing an operator or equals button while in
    "display-ans" state would cause number data to
    be wildly innaccurate, and several others that
    cropped up while fixing those bugs.
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -20,13 +20,16 @@ equals.addEventListener("click", completeExpression);
 const dec = document.querySelector("#dec");
 dec.addEventListener("click", concatDec);
 
+const back = document.querySelector("#back");
+back.addEventListener("click", backspace);
+
 //functions for arithmetic operations
 function add(a,b) { return a + b; }
 function subtract(a,b) { return a - b; }
 function multiply(a,b) { return a * b; }
 function divide(a,b) { 
     if(b===0)
-        return "Div by zero"
+        return "D.B.Z."
     return a / b; 
 }
 
@@ -36,7 +39,6 @@ function getState() {
 
 //evalutates expression based on operator
 function operate(op, a, b) {
-    console.log(`${a}, ${b}`)
     switch(op){
         case "+":
             return add(a,b);
@@ -51,6 +53,7 @@ function operate(op, a, b) {
     }
 }
 
+//adds digit to display if there's room
 function concatDigit(e){
     if(state === "init" || state ==="display-ans") {
         clearAll();
@@ -62,9 +65,13 @@ function concatDigit(e){
     }
     const pressed = e.target;
     let number = pressed.value;
-    display.innerHTML = display.innerHTML + number;
+    if(display.innerHTML.length < 11)
+        display.innerHTML = display.innerHTML + number;
+    else
+        triggerOverflow();
 }
 
+//adds decimal place to display if there's room
 function concatDec(e){
     if(state === "init" || state ==="display-ans") {
         clearAll();
@@ -74,11 +81,15 @@ function concatDec(e){
         clearDisplay();
         state ="input-right"
     }
-    if(!display.innerHTML.includes("."));
-        display.innerHTML = display.innerHTML + ".";
+    if(!display.innerHTML.includes(".")){
+        if(display.innerHTML.length < 11)
+            display.innerHTML = display.innerHTML + ".";
+        else if (display.innerHTML.length >= 11)
+            triggerOverflow();
+    }
 }
 
-//
+
 function resetValues(){
     leftNum = undefined;
     rightNum = undefined;
@@ -93,15 +104,13 @@ function clearDisplay() {
 function clearAll(e) {
     clearDisplay();
     resetValues();
+    state = "init";
 }
 
 function evaluate (){
     if(!opSet)
         return "";
     ans = operate(operator, leftNum, rightNum)
-    if (ans === "Div by zero")
-        resetValues();
-    console.log(ans);
     return ans;
 }
 
@@ -114,9 +123,9 @@ function inputNum() {
         num = parseFloat(input);
     else
         num = parseInt(input);
-    if(state === "input-left" || state === "display-ans"){
+    console.log(num);
+    if(state === "input-left"){
         leftNum = num;
-        console.log(num);
     }
     else if(state==="input-right")
         rightNum = num;
@@ -133,24 +142,99 @@ function setOperator(e) {
         resetValues();
         display.innerHTML = answer;
         leftNum = answer;
-        state === "display-ans";
+        state = "display-ans";
     }
-    if(state  === "input-left" || state === "display-ans")
+    else if(state  === "input-left")
     {
         inputNum();
         state = "input-mid";
     }
+    else if(state === "display-ans"){
+        state = "input-mid";
+    }
+    else if(state === "init")
+        return;
     const pressed = e.target;
     operator = pressed.value;
     opSet = true;
 }
+
+//Displays overflow and resets values;
+function triggerOverflow(){
+    console.log(leftNum + " " + rightNum)
+    resetValues();
+    state = "init";
+    display.innerHTML = "Overflow";
+}
+
+//rounds float based on how much space there is
+function trimFloat(num){
+    wholeNumLength = num.toFixed(0).toString().length;
+    maxDecLength = Math.max( 0 , 10-wholeNumLength);
+    return trimZeroes(num.toFixed(maxDecLength));
+}
+
+//Trims unnecessary floating zeroes
+function trimZeroes(num){
+    //convert to string
+    numString = num.toString();
+    //check if number is a float (if not do nothing)
+    if (numString.includes(".")) {
+        //iterates over string from end deleting zeroes
+        //until it comes to a non-zero number or decimal point
+        
+        for(let i = numString.length - 1; i >= 0; i--){
+            if(numString[i] == "0"){
+                numString=numString.substring(0,i-1);
+            }
+            else if(numString[i] == "."){
+                numString=numString.substring(0,i-1);
+                break;
+            }
+            else
+                break;
+        }
+    }
+    //convert back to float
+    return parseFloat(numString);
+}
  
+//Calculates expression answer then displays
+//number that fits in the space available
+//or displays "Overflow" if answer is too big
 function completeExpression(e) {
     if(state === "input-right"){
         inputNum();
-        display.innerHTML = evaluate();
+        ans = evaluate();
+        if(ans <= 99999999999)
+        {
+            ans=trimFloat(ans)
+            display.innerHTML = ans;
+        }
+        else {
+            triggerOverflow();
+            return;
+        }
         resetValues();
+        state = "input-left";
         inputNum();
-        state = "display-ans";
+        if(ans !== "D.B.Z.")
+            state = "display-ans";
+        else
+            state = "init"
+    }
+}
+
+function backspace(e){
+    console.log("backspace");
+    if(state === "input-left" || state === "input-right"){
+        str = display.innerHTML;
+        if(str.length === 1 || str.length === 0){
+            display.innerHTML = "";
+        }
+        else{
+            str = str.substring(0,(str.length-1));
+            display.innerHTML = str;
+        }
     }
 }
